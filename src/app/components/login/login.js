6df2ac7b9cbac68d8b75238e37fb56ea1774d7b4
honger05 <honger05@126.com>
2016-01-26 16:15:10 +0800

    f

--- src/app/components/login/login.js
+++ src/app/components/login/login.js
@@ -1,18 +1,21 @@
 
 var Utils = require('utils')
 require('../../common/common.scss')
+require('./login.scss')
+
 var RSAUtils = require('./rsautils')
 var CEIS = {}
 
 var RSA_KEY_URL = Utils.CONTEXT_URL + 'getrsakey.do'
 var LOGIN_URL = Utils.CONTEXT_URL + 'login'
+var VALIDATA_IMG_URL = 'ceis/servlet/validateCodeServlet'
 
-$.post(RSA_KEY_URL, {}, function(data) {
-  if (data && data.content) {
-    CEIS.exponent = data.content.e;
-    CEIS.modulus = data.content.n;
-  }
-})
+// $.post(RSA_KEY_URL, {}, function(data) {
+//   if (data && data.content) {
+//     CEIS.exponent = data.content.e;
+//     CEIS.modulus = data.content.n;
+//   }
+// })
 
 $('#toast').on('opened.modal.amui', function() {
   setTimeout(function() {
@@ -20,9 +23,14 @@ $('#toast').on('opened.modal.amui', function() {
   }.bind(this), 1000)
 })
 
-$('#account').on('change', function() {
+$('#username').on('change', function() {
   var $username = $(this);
   $username.val($.trim($username.val()).toUpperCase());
+})
+
+$('#validateCodeDiv').on('click', 'a', function() {
+  var img = $(this).prev()[0]
+  img.src = VALIDATA_IMG_URL + '?' + Math.random()
 })
 
 var $loginBtn = $('#loginBtn')
@@ -40,17 +48,23 @@ function effectDone() {
 $('#loginForm').submit(function(ev) {
   ev.preventDefault()
 
-  var $account = $('#account')
+  var $username = $('#username')
   var $password = $('#password')
+  var $validate = $('#validateCode')
   var pwd_str = $.trim($password.val()),
-      act_str = $.trim($account.val())
+      act_str = $.trim($username.val()),
+      vali_str = $.trim($validate.val())
 
   if (act_str === '') {
-    $('#toast-cnt').html('请输入账号')
+    $('#toast-cnt').html('请填写用户名')
     $('#toast').modal('open')
   }
   else if (pwd_str === '') {
-    $('#toast-cnt').html('请输入密码')
+    $('#toast-cnt').html('请填写密码')
+    $('#toast').modal('open')
+  }
+  else if ($('#validateCodeDiv').is(':visible') && vali_str === '') {
+    $('#toast-cnt').html('请填写验证码')
     $('#toast').modal('open')
   }
   else {
@@ -63,30 +77,36 @@ $('#loginForm').submit(function(ev) {
       $password.val(encryptedPwd);
     }
 
-    $.post(LOGIN_URL, $loginForm.serializeArray(), function(data) {
-      if (data && data.sessionid) {
-        CEIS.sessionid = data.sessionid;
-        CEIS.firstLogin = data.firstLogin;
-        if (data.firstLogin) {
-          $('#toast-cnt').html('首次登陆或密码过期，请修改密码！')
-          $('#toast').modal('open')
-        } else {
-          $('#toast-cnt').html('登录成功！')
-          $('#toast').modal('open')
-        }
-      }
-      else {
-        $password.val('')
-        $('#toast-cnt').html('账号或密码错误！')
-        $('#toast').modal('open')
-        effectDone()
-      }
-    })
+    // $.post(LOGIN_URL, $loginForm.serializeArray(), function(data) {
+    //   if (data && data.sessionid) {
+    //     CEIS.sessionid = data.sessionid;
+    //     CEIS.firstLogin = data.firstLogin;
+    //     if (data.firstLogin) {
+    //       $('#toast-cnt').html('首次登陆或密码过期，请修改密码！')
+    //       $('#toast').modal('open')
+    //     }
+    //     else {
+    //       $('#toast-cnt').html('登录成功！')
+    //       $('#toast').modal('open')
+    //     }
+    //   }
+    //   else {
+    //     $password.val('')
+    //     $('#toast-cnt').html(data.message)
+    //     $('#toast').modal('open')
+    //     effectDone()
+    //
+    //     if (data.shiroLoginFailure == 'org.apache.shiro.authc.AuthenticationException'){
+    //       $('#validateCodeDiv').show();
+    //     }
+    //     $('#validateCodeDiv a').click();
+    //   }
+    // })
 
-    // setTimeout(function() {
-    //   $.AMUI.progress.done()
-    //   document.location.href = 'index.html'
-    // }, 1500)
+    setTimeout(function() {
+      $.AMUI.progress.done()
+      document.location.href = 'index.html'
+    }, 1500)
   }
 
 })
