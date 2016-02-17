5a3a54c64737208067c0a64869777ad4c697e740
honger05 <honger05@126.com>
2016-02-17 12:10:16 +0800

    f

--- src/app/components/dowithheld/dowithheld.js
+++ src/app/components/dowithheld/dowithheld.js
@@ -24,44 +24,59 @@ Utils.Utilities.unload(function() {
   Utils.Storage.set(Utils.Storage.PAY_SESSION)
 })
 
-$.post(Utils.URL.REPAY_DETAIL, pay_session)
+var data_detail = {}
+
+requestDetail()
+
+$('#searchForm').submit(function(ev) {
+  ev.preventDefault()
+  var payment_val = $.trim($('#payment').val())
+
+  if (payment_val === '') {
+    Utils.UI.toast('请输入扣款金额')
+  }
+  else if (!Utils.REG.isDigit(payment_val)) {
+    Utils.UI.toast('请输入合法金额')
+  }
+  else {
+    $('#confirm-modal').modal({
+      onConfirm: function(options) {
+        confirmPay(payment_val)
+      }
+    });
+  }
+})
+
+function confirmPay(payment_val) {
+  $('#smtBtn').button('loading')
+  $.post(Utils.URL.REPAY_DEBIT, {
+    objectNo: pay_session.objectNo,
+    repayType: pay_session.repayType,
+    payMoney: Number(payment_val)
+  })
   .done(function(data) {
     if (data.status === 'success') {
-      var _html = Handlebars.compile(require('./detail.hbs'))(data.content)
-      $('#main').append(_html)
-    }
-  })
-
-  $('#searchForm').submit(function(ev) {
-    ev.preventDefault()
-    var payment_val = $.trim($('#payment').val())
-
-    if (payment_val === '') {
-      Utils.UI.toast('请输入扣款金额')
-    }
-    else if (!Utils.REG.isDigit(payment_val)) {
-      Utils.UI.toast('请输入合法金额')
-    }
-    else {
-      $('#confirm-modal').modal({
-        onConfirm: function(options) {
-          confirmPay(payment_val)
-        }
-      });
-    }
-  })
-
-  function confirmPay(payment_val) {
-    $('#smtBtn').button('loading')
-    $.post(Utils.URL.REPAY_DEBIT, {
-      objectNo: pay_session.objectNo,
-      repayType: pay_session.repayType,
-      payMoney: Number(payment_val)
-    })
-    .done(function(data) {
       Utils.UI.toast(data.msg)
+      $('#payment').val('')
+      if (Number(payment_val) !== Number(data_detail.repayAmount)) {
+        requestDetail()
+      } else {
+        $('#chevron-left').click()
+      }
+    }
+  })
+  .always(function() {
+    $('#smtBtn').button('reset')
+  })
+}
+
+function requestDetail() {
+  $.post(Utils.URL.REPAY_DETAIL, pay_session)
+    .done(function(data) {
+      if (data.status === 'success') {
+        data_detail = data.content
+        var _html = Handlebars.compile(require('./detail.hbs'))(data.content)
+        $('#main').append(_html)
+      }
     })
-    .always(function() {
-      $('#smtBtn').button('reset')
-    })
-  }
+}
